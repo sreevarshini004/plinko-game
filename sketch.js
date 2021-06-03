ff16c7a2a47cdd351b55e5d97462adb6f7ee3951
sreevarshini004 <77574823+sreevarshini004@users.noreply.github.com>
2021-06-03 17:29:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,99 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+  var particles = [];
+  var plinkos = [];
+  var divisions =[];
+  var divisionHeight=300;
+  var score =0;
+   
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+  //create division objects
+  for (var k = 0; k <=80; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 0; k <=180; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 0; k <=280; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 0; k <=380; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 0; k <=480; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 0; k <=580; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 0; k <=680; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 0; k <=780; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  //create 1st row of plinko objects
+  for (var j = 40; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,75));
+  }
+
+  //create 2nd row of plinko objects
+  for (var j = 15; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,175));
+  }
+
+  //create 3rd row of plinko objects
+  for (var j = 40; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,275));
+  } 
+
+  
+  //create 4th row of plinko objects
+  for (var j = 15; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,375));
+  }
+
+  //create particle objects
+  
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ 
+  Engine.update(engine);
+  ground.display();
+
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-10,width/2+10),10,10));
+  }
+  
+  //display the plinkos 
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();   
+  }
+   
+  //display the divisions
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display();
+  }
+
+  //display the paricles 
+  for (var l = 0; l< divisions.length; l++){
+    divisions[l].display();
+  }
+  
+}
